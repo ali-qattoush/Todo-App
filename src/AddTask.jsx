f35bb43d44bfe862f00a79ec8ad664eca6e8fb0b
ali-qattoush <aliqattoushwork@gmail.com>
2023-08-16 10:36:46 +0300

    functional Component Rework

--- src/AddTask.jsx
+++ src/AddTask.jsx
@@ -1,44 +1,31 @@
-import React from 'react';
 import './App.css';
+import { useState } from 'react';
 
+function AddTask( handleAddTask ) {
+  const [task, setTask] = useState('');
 
-class AddTask extends React.Component {
-    constructor() {
-      super();
-      this.state = {
-        task: '',
-      };
+  const handleChange = (event) => {
+    setTask(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (task === '') {
+      alert("Empty submission not allowed");
+    } else {
+      handleAddTask(task);
+      setTask('');
     }
-    
-  
-  
-    handleChange = (event) => {
-      this.setState({ task: event.target.value });
-    };
-  
-    handleSubmit = (event) => {
-      event.preventDefault();
-      console.log("thisss")
-      if (this.state.task === '') {
-        alert("Empty submission not allowed")
-      } else {
-        // eslint-disable-next-line react/prop-types
-        this.props.handleAddTask(this.state.task); 
-        this.setState({ task: '' });
-      }
+  };
 
-    };
-  
-    render() {
-      return (
-        <div className='addtask'>
-          <form className='addtaskelements'>
-            <button className='submitbutton' onClick={this.handleSubmit}>+</button>
-            <input className='inputbar' type="text" placeholder="Add Task" value={this.state.task} onChange={this.handleChange} />
-          </form>
-        </div>
-      );
-    }
+  return (
+    <div className='addtask'>
+      <form className='addtaskelements'>
+        <button className='submitbutton' onClick={handleSubmit}>+</button>
+        <input className='inputbar' type="text" placeholder="Add Task" value={task} onChange={handleChange} />
+      </form>
+    </div>
+  );
 }
 
 export default AddTask;
--- src/TodoClass.jsx
+++ src/TodoClass.jsx
@@ -1,55 +1,43 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
 import AddTask from './AddTask.jsx';
 import { v4 as uuidv4 } from 'uuid';
 
-class TODO extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      data: {
-        tasks: [
-          { id: uuidv4(), text: "Fetch groceries" },
-          { id: uuidv4(), text: "Clean the kitchen" },
-          { id: uuidv4(), text: "Buy new chair" },
-          { id: uuidv4(), text: "Wash the car" },
-        ],
-        doneTasks: [],
-      }
-    };
-  }
+function TODO() {
+  const [data, setData] = useState({
+    tasks: [
+      { id: uuidv4(), text: "Fetch groceries" },
+      { id: uuidv4(), text: "Clean the kitchen" },
+      { id: uuidv4(), text: "Buy new chair" },
+      { id: uuidv4(), text: "Wash the car" },
+    ],
+    doneTasks: [],
+  });
 
-  handleAddTask = (task) => {
+  const handleAddTask = (task) => {
     const newTask = { id: uuidv4(), text: task };
-    console.log("test")
-    this.setState((prevState) => ({
-      data: {
-        ...prevState.data,
-        tasks: [...prevState.data.tasks, newTask],
-      },
+    setData((prevState) => ({
+      ...prevState,
+      tasks: [...prevState.tasks, newTask],
     }));
   };
 
-  
-  DeleteTask = (id) => {
-    this.setState((prevState) => {
-      const { tasks, doneTasks } = prevState.data;
+  const DeleteTask = (id) => {
+    setData((prevState) => {
+      const { tasks, doneTasks } = prevState;
       const updatedTasks = tasks.filter((item) => item.id !== id);
       const updatedDoneTasks = doneTasks.filter((task) => task.id !== id);
       return {
-        data: {
-          tasks: updatedTasks,
-          doneTasks: updatedDoneTasks,
-        },
+        tasks: updatedTasks,
+        doneTasks: updatedDoneTasks,
       };
     });
   };
 
-  CheckboxChange = (id, isDoneTask) => {
-    this.setState((prevState) => {
-      const updatedData = { ...prevState.data };
+  const CheckboxChange = (id, isDoneTask) => {
+    setData((prevState) => {
+      const updatedData = { ...prevState };
       const task = updatedData.tasks.find((task) => task.id === id) || updatedData.doneTasks.find((task) => task.id === id);
-    
 
       if (isDoneTask) {
         updatedData.doneTasks = updatedData.doneTasks.filter((task) => task.id !== id);
@@ -59,16 +47,15 @@ class TODO extends React.Component {
         updatedData.doneTasks = [...updatedData.doneTasks, task];
       }
 
-      return { data: updatedData };
+      return updatedData;
     });
   };
 
-  render() {
-    const { tasks, doneTasks } = this.state.data;
+  const { tasks, doneTasks } = data;
 
-    return (
-      <div className='container'>
-        <div className='todoContainer'>
+  return (
+    <div className='container'>
+      <div className='todoContainer'>
         <h1 className='todo'>To Do :</h1>
         {tasks.map((task) => (
           <div className='tasks' key={task.id}>
@@ -78,48 +65,39 @@ class TODO extends React.Component {
                 type='checkbox'
                 value={task.id}
                 checked={false}
-                onChange={() => this.CheckboxChange(task.id, false)}
+                onChange={() => CheckboxChange(task.id, false)}
               />
               <span className="checkbox-text">{task.text}</span>
             </label>
-            <button className='deleteButton' onClick={() => this.DeleteTask(task.id)}>
+            <button className='deleteButton' onClick={() => DeleteTask(task.id)}>
               <i className="fas fa-trash"></i>
-
-             
             </button>
           </div>
         ))}
-        </div>
-        <div className='doneContainer'>
-          <h1 className='todo'>Done :</h1>
-  
-          {doneTasks.map((task) => (
-            <div className='doneTasks' key={task.id}>
-              <label className='checkboxlabel'>
-                <input
-                  className='checkboxclass'
-                  type='checkbox'
-                  value={task.id}
-                  checked={true}
-                  onChange={() => this.CheckboxChange(task.id, true)}
-                />
-                <span className="checkbox-text">{task.text}</span>
-                
-              </label>
-              <button className='deleteButton' onClick={() => this.DeleteTask(task.id)}>
-                <i className="fas fa-trash"></i>
-              
-              </button>
-            </div>
-          ))}
-        </div>
-        <AddTask handleAddTask={this.handleAddTask} />
       </div>
-    );
-  }
-
-  
-
+      <div className='doneContainer'>
+        <h1 className='todo'>Done :</h1>
+        {doneTasks.map((task) => (
+          <div className='doneTasks' key={task.id}>
+            <label className='checkboxlabel'>
+              <input
+                className='checkboxclass'
+                type='checkbox'
+                value={task.id}
+                checked={true}
+                onChange={() => CheckboxChange(task.id, true)}
+              />
+              <span className="checkbox-text">{task.text}</span>
+            </label>
+            <button className='deleteButton' onClick={() => DeleteTask(task.id)}>
+              <i className="fas fa-trash"></i>
+            </button>
+          </div>
+        ))}
+      </div>
+      <AddTask handleAddTask={handleAddTask} />
+    </div>
+  );
 }
 
 export default TODO;
